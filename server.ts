e9bedc164cc8fe02711ece0caf7d1540e03710bc
chainguru1014 <chainguru1014@users.noreply.github.com>
2026-02-12 09:21:35 -0800

    api url update

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ mongoose.connection.on('error', (error: any) => {
 });
 // Start the server
 const port = process.env.PORT || 8080;
-const server = app.listen(port, () => {
+const server = app.listen(port, "0.0.0.0", () => {
     console.log(`Application is running on port ${port}`);
 });
 
